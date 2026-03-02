b9232f1bc46d3e82850c227aa69721df503a39b2
Your Name <cuentauniagus@gmail.com>
2026-03-02 00:48:57 -0300

    reemplazos

--- memoria-compartida-sisop-2024-master/problemas-varios/2024/normal/ej3/codigo2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char* argv[]) 
-{
-    char *a = argv[1];
-    printf("El valor de a es: %s\n", a);
-    sleep(10); // PARA PODER CAPTURAR EL ARBOL DE PROCESOS
-}
